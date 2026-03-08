84df303632831b59d7dc0b4afa547ad3ac2f0750
Raghvendra Singh <raghavsingh99199@gmail.com>
2026-03-09 01:09:29 +0530

    ejs intigrated!

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
     res.render('index');
+});
+app.get('/profile/:id', (req, res) => {
+    res.send(`Profile of user with ID: ${req.params.id}`);
 });
 
 app.listen(3000, () => {
